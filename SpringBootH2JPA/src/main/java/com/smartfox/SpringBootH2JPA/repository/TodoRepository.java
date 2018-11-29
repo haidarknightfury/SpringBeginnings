1b086d63b3eb17785f33458dc5d2b1e104dcb51c
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-11-29 21:00:48 +0400

    add correct dependencies

--- SpringBootH2JPA/src/main/java/com/smartfox/SpringBootH2JPA/repository/TodoRepository.java
+++ SpringBootH2JPA/src/main/java/com/smartfox/SpringBootH2JPA/repository/TodoRepository.java
@@ -1,11 +1,12 @@
 package com.smartfox.SpringBootH2JPA.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 import com.smartfox.SpringBootH2JPA.model.Todo;
 
-@Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+public interface TodoRepository extends JpaRepository<Todo, Long> , QueryDslPredicateExecutor<Todo>{
 
 }
