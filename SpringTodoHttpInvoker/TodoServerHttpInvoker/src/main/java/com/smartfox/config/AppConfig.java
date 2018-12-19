bb3b82dd56ec9dbc5fc8714ee90906283bd92490
Haidar <hdargaye@maureva.com>
2018-12-19 17:08:45 +0400

    add spring todo http invoker - to complete client part

---
+++ SpringTodoHttpInvoker/TodoServerHttpInvoker/src/main/java/com/smartfox/config/AppConfig.java
@@ -0,0 +1,41 @@
+package com.smartfox.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+
+import com.smartfox.service.TodoService;
+
+@Configuration
+@ComponentScan(basePackages = "com.smartfox")
+public class AppConfig {
+
+    /**
+     * Work as a controller - MVC Controller - receives request from the client through DispatcherServlet and translate those requests
+     * to method calls on the service implementation pojo
+     */
+    @Bean
+    public HttpInvokerServiceExporter httpInvokerServiceExporter(TodoService todoService) {
+        HttpInvokerServiceExporter exporter = new HttpInvokerServiceExporter();
+        exporter.setService(todoService);
+        exporter.setServiceInterface(TodoService.class);
+        return exporter;
+    }
+
+    /**
+     * Dispatcher must be able to handle *.service
+     */
+    @Bean
+    public HandlerMapping httpInvokerMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        Properties mappings = new Properties();
+        mappings.setProperty("/todo.service", "httpInvokerServiceExporter");
+        mapping.setMappings(mappings);
+        return mapping;
+    }
+}
---
+++ SpringTodoHttpInvoker/TodoServerHttpInvoker/src/main/java/com/smartfox/service/TodoService.java
@@ -0,0 +1,5 @@
+package com.smartfox.service;
+
+public interface TodoService {
+
+}
